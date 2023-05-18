54bbd540e8873a66de15d16facbaaa6cd86ee5a1
sandraosr25 <sandraosr@gmail.com>
2023-05-18 07:14:21 -0500

    first commit

---
+++ src/pages/Comics.js
@@ -0,0 +1,7 @@
+export default function Comics() {
+    return (
+        <div>
+            <p>Estamos en la página de Comics</p>
+        </div>
+    );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <p>Estamos en Home</p>
+        </div>
+    );
+}
