b021ba9b1273f9dee7c0ebc4acae0d7e94da1b0c
sandraosr25 <sandraosr@gmail.com>
2023-05-19 07:56:22 -0500

    Ajustes en Header manejo del menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,26 @@
 import './Header.scss';
 import {Menu} from 'semantic-ui-react';
+import { useState } from 'react';
+import {useNavigate, useLocation} from 'react-router-dom'; 
 
 export default function Header() {
+    const currentPath = useLocation();
+    console.log(currentPath);
+    const finalCurrentPath = currentPath.pathname.replace("/","");
+    const [activeItem, setActiveItem] = useState("");
+    const history = useNavigate();
+
+    const itemclic = (e,{name}) => {
+       setActiveItem(name);
+       history(name, {replace:true});
+    };
+
  return (
     <div className="header-menu">
         <Menu secondary>
-            <Menu.Item name="inicio" onClick={()=>console.log("haciendo clic en el menu")} />
-            <Menu.Item name="series" onClick={()=>console.log("haciendo clic en series")} />
-            <Menu.Item name="comics" onClick={()=>console.log("haciendo clic en comics")} />
+            <Menu.Item name="inicio" active={activeItem==="inicio"} onClick={itemclic} />
+            <Menu.Item name="series" active={activeItem==="series"} onClick={itemclic} />
+            <Menu.Item name="comics" active={activeItem==="comics"} onClick={itemclic} />
         </Menu>
     </div>
 
