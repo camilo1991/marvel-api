3175f1251269fb374ef05d440f3dc0b287045f5c
sandraosr25 <sandraosr@gmail.com>
2023-05-26 09:26:12 -0500

    trabajo en páginas Series y Comics

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import { Grid, Header } from "semantic-ui-react";
+import Container from "../components/Container";
+import ListComics from "../components/ListComics";
+import useFetch from "../hooks/useFetch";
+
 export default function Comics() {
+    const [renderComics, setRenderComics] = useState(10);
+    const listComics = useFetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=9e2ffd6f166951a66ec8abb1db3cf828&hash=961282844f340b77979afff0813190bb&limit=10');
+    console.log(listComics);
     return (
-        <div>
-            <p>Estamos en la página de Comics, pronto haremos unos cambios</p>
+        <div className="comics-page">
+            <div id="slide-comics-image" />
+            <Grid>
+                <Grid.Column>
+                    <Container bg="light">
+                        <Header as="h2">Los mejores Comics</Header>
+                        <ListComics
+                            listComics={listComics}
+                            renderComics={renderComics}
+                            setRenderComics={setRenderComics}
+                        />
+                    </Container>
+                </Grid.Column>
+            </Grid>
         </div>
     );
 }
